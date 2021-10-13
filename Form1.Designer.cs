6e8ee086795c556d9df37255bc48ace405ab0779
Ingeinlens <ingeinlens@gmail.com>
2021-10-13 16:33:10 +0800

    Восстановление программы после ошибки

---
+++ Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace Lab2_Gui
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // price
+            // 
+            this.price.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.price.Location = new System.Drawing.Point(23, 195);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(332, 34);
+            this.price.TabIndex = 0;
+            this.price.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.price_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(19, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(406, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Укажите стоимость товара (в копейках)";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(209, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(23, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(144, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(15, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Задание 1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(637, 617);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Bisque;
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.price);
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(629, 480);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Задание 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(130, 339);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(112, 28);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "пока пусто";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(23, 339);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Результат:";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(17, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(466, 71);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Дано натуральное число 1 < n < 9999 определяющее стоимость товара в копейках. Выр" +
+    "азить стоимость в рублях и копейках, например, 3 рубля 21 копейка, 15 рублей 5 к" +
+    "опеек, 1 рубль ровно и т. п.";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.Bisque;
+            this.tabPage2.Controls.Add(this.label10);
+            this.tabPage2.Controls.Add(this.label9);
+            this.tabPage2.Controls.Add(this.label8);
+            this.tabPage2.Controls.Add(this.label7);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(629, 584);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Задание 2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(18, 375);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(128, 20);
+            this.label10.TabIndex = 4;
+            this.label10.Text = "Величина вклада";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 289);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(128, 20);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "Величина вклада";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 210);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Величина вклада";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(18, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(591, 84);
+            this.label7.TabIndex = 1;
+            this.label7.Text = resources.GetString("label7.Text");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(15, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 37);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Задание 2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 641);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Лабораторная №2 GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_Gui
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int price;
+            try
+            {
+                price = int.Parse(this.price.Text);
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+
+            string message = Logic.Сonverter(price);
+            label5.Text = message;
+        }
+
+
+        private void price_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+
+        public class Logic
+        {
+            public static string Сonverter(int price)
+            {
+                //просчитывание стоимости товара в рублях и копейках
+                int penny = price % 100;
+                int rubles = price / 100;
+
+                //объявление переменных отвечающих за правильное название стоимостей
+                string rublesName;
+                string pennyName;
+
+                //проверка условия, если 1, то рубль, если 2-4, то рубля, если 5, то рублей и так далее, для всех чисел
+                if ((rubles % 10) == 1 && rubles != 11)
+                {
+                    rublesName = "рубль";
+                }
+                else if (2 <= (rubles % 10) && (rubles % 10) <= 4 && rubles != 12 && rubles != 13 && rubles != 14)
+                {
+                    rublesName = "рубля";
+                }
+                else
+                {
+                    rublesName = "рублей";
+                }
+
+                //такая же проверка, только для копеек
+                if ((penny % 10) == 1 && penny != 11)
+                {
+                    pennyName = "копейка";
+                }
+                else if (2 <= (penny % 10) && (penny % 10) <= 4 && penny != 12 && penny != 13 && penny != 14)
+                {
+                    pennyName = "копейки";
+                }
+                else
+                {
+                    pennyName = "копеек";
+                }
+
+                //вывод стоимости с проверкой, если 0 рублей, то только цена в копейках,
+                //если 0 копеек, то N рублей ровно или вывод всей стоимости
+                string message = " ";
+                if (rubles == 0)
+                {
+                    message = penny + " " + pennyName;
+                }
+                else if (penny == 0)
+                {
+                    message = rubles + " " + rublesName + " ровно";
+                }
+                else
+                {
+                    message = rubles + " " + rublesName + " " + penny + " " + pennyName;
+                }
+                return message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            price.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+    }
+}
